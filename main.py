268ffa3b414f7226942d07e2a9b48195d8ecd2e1
Sumo Akula <sumoa29@gmail.com>
2021-05-11 13:26:12 -0400

    connect to local mongodb

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
+
 import discord
 from discord.ext import commands
+import pymongo
 
 import secret  # super duper secret .py file used to store the bot token
 
 activity = discord.Activity(type=discord.ActivityType.listening, name="Suge")
 bot = commands.Bot(command_prefix=".", activity=activity)
+
+# connect to database
+mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongoclient["botdb"]
 
 for f in os.listdir("./cogs"):
     if f.endswith(".py"):
