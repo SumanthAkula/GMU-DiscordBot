1e57073ecc9229946138711983a23af0993e1414
Sumo Akula <sumoa29@gmail.com>
2021-05-11 01:03:17 -0400

    added cogs for organization

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import os
 import discord
 from discord.ext import commands
 
 import secret  # super duper secret .py file used to store the bot token
 
-bot = commands.Bot(command_prefix=".")
+activity = discord.Activity(type=discord.ActivityType.listening, name="Suge")
+bot = commands.Bot(command_prefix=".", activity=activity)
+
+for f in os.listdir("./cogs"):
+    if f.endswith(".py"):
+        bot.load_extension(f"cogs.{f[:-3]}")
 
 
 @bot.event
 async def on_ready():
-    print("bot started!")
+    print("LETS GOOOOOO!")
 
 
 @bot.command()
