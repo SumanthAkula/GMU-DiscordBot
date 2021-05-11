c68ae2d4ef30b290b94e2a5d1220c647b88c143b
Sumo Akula <sumoa29@gmail.com>
2021-05-11 00:06:35 -0400

    created a basic ass starting bot

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("69")
+import discord
+from discord.ext import commands
+
+import secret
+
+bot = commands.Bot(command_prefix=".")
+
+
+@bot.event
+async def on_ready():
+    print("bot started!")
+
+
+bot.run(secret.TOKEN)
