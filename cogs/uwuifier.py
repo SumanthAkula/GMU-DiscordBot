5501be5fb511b8b506e2ddbafa71c60d76250d1d
Sumo Akula <sumoa29@gmail.com>
2021-05-11 01:03:42 -0400

    this is just cursed af

---
+++ cogs/uwuifier.py
@@ -0,0 +1,16 @@
+import uwuify
+from discord.ext import commands
+
+
+class Uwuifier(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def uwu(self, ctx: commands.Context, *, msg):
+        await ctx.send(uwuify.uwu(msg))
+        await ctx.message.delete()
+
+
+def setup(bot):
+    bot.add_cog(Uwuifier(bot))
