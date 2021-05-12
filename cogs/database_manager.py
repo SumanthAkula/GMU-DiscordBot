c36a9a21d3f5a92b888b42be3f9cdba5931114a1
Sumo Akula <sumoa29@gmail.com>
2021-05-12 16:00:00 -0400

    created this to deal with collection creation when the bot joins a guild

--- cogs/database_manager.py
+++ cogs/database_manager.py
@@ -7,24 +7,24 @@ class DatabaseManager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @commands.Cog.listener
-        async def on_guild_join(self, guild: discord.Guild):
-            """
-            check if a collection exists for this guild.  if it does, do nothing.  otherwise create the collection
-            and create empty fields
-            """
-            if f"GUILD_{guild.id}" in db.list_collection_names():
-                return
-            collection = db[f"GUILD_{guild.id}"]
-            guild_id = {
-                "guild_id": guild.id
-            }
-            collection.inesrt_one(guild_id)
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: discord.Guild):
+        """
+        check if a guild_collection exists for this guild.  if it does, do nothing.
+        otherwise create the guild_collection and create empty fields
+        """
+        if f"GUILD_{guild.id}" in db.list_collection_names():
+            return
+        guild_collection = db[f"GUILD_{guild.id}"]
+        guild_id = {
+            "guild_id": guild.id
+        }
+        guild_collection.insert_one(guild_id)
 
-        @commands.command()
-        async def force_collection_creation(self, ctx):
-            await on_guild_join(ctx.guild)
-            await ctx.send(f"Guild with ID {ctx.guild.id} added to database")
+    # @commands.command()
+    # async def force_collection_creation(self, ctx):
+    #     await self.on_guild_join(ctx.guild)
+    #     await ctx.send(f"Guild with ID {ctx.guild.id} added to database")
 
 
 def setup(bot):
