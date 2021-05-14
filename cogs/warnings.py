338c2a7fcbf8df9d85af9398b6f9fc9a3687fcf2
Sumo Akula <sumoa29@gmail.com>
2021-05-14 01:37:16 -0400

    made the get warnings method not suck

--- cogs/warnings.py
+++ cogs/warnings.py
@@ -15,7 +15,6 @@ class Warnings(commands.Cog):
     async def __add_warning_cmd(self, ctx: commands.Context, member: discord.Member, points: int,
                                 *, reason: str = "They were being a silly baka"):
         warn_id = await self.add_warning(ctx.guild.id, member.id, points, reason)
-        warn_count = await self.get_warnings_for_user(ctx.guild.id, member.id)
         await ctx.send(f":warning: [`{warn_id}`] {member.mention}, you have been given a {points} point warning with "
                        f"the reason \"{reason}\"\n"
                        f"Fuck up one more time and I send the secret girls to break your knee caps")
@@ -32,32 +31,24 @@ class Warnings(commands.Cog):
 
     @commands.command(name="warncount", aliases=["wc"])
     async def __get_warning_count(self, ctx: commands.Context, member: discord.Member):
-        warn_count = await self.get_warnings_for_user(ctx.guild.id, member.id)
+        warnings = await self.get_warnings_for_user(ctx.guild.id, member.id)
+        warn_count = len(warnings)
         await ctx.send(f"{member.display_name} has {warn_count} warning(s)")
 
     @staticmethod
-    async def get_warnings_for_user(guild_id: int, member_id: int) -> int:
-        guild_warnings = main.db.warnings.find(
+    async def get_warnings_for_user(guild_id: int, member_id: int) -> list:
+        member_warnings = main.db.warnings.find(
             {
                 "guild_id": guild_id,
                 "user_id": member_id
             }
         )
-        count = 0
-        for w in guild_warnings:
-            print(w)
-            count += 1
-        return count
+        return list(member_warnings)
 
     @staticmethod
     async def get_most_recent_warning(guild_id: int, member_id: int):
-        guild_collection = main.db[f"GUILD_{guild_id}"]
-        warnings = dict(guild_collection.find_one({}))["warnings"]
-        member_warnings = []
-        for warning in warnings:
-            if warning["user_id"] == member_id:
-                member_warnings.append(warning)
-        return member_warnings[-1]
+        warnings = await Warnings.get_warnings_for_user(guild_id, member_id)
+        return warnings[-1]
 
     @staticmethod
     async def add_warning(guild_id, member_id, points: int, reason: str) -> str:
