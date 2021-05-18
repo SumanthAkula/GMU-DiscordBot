ccae2b2c8e2429b1c1ecf4881b2935a8edf5820e
Sumo Akula <sumoa29@gmail.com>
2021-05-18 17:43:15 -0400

    disabled spam detection in DM and fixed the warning thingy

--- cogs/spam_detector.py
+++ cogs/spam_detector.py
@@ -13,6 +13,8 @@ class SpamDetector(commands.Cog):
     async def on_message(self, message: discord.Message):
         if message.author == self.bot.user:
             return
+        if message.guild is None:
+            return  # message was sent in DM, and we dont need to check for spam in DMs
         member_messages = []
         for m in await message.channel.history(limit=10, before=message, oldest_first=False).flatten():
             if m.author.id == message.author.id and m.guild.id == message.guild.id:
@@ -22,23 +24,14 @@ class SpamDetector(commands.Cog):
         if message.created_at.timestamp() - member_messages[-1].created_at.timestamp() < 10:
             await self.warn(message)
 
-    @staticmethod
-    async def warn(message: discord.Message):
-        start = time.time()
-        warned = False
+    async def warn(self, message: discord.Message):
         recent = await Warnings.get_most_recent_warning(message.guild.id, message.author.id)
         if recent is None or time.time() - recent["time"] > 10:
             # user has either never been warned before or hasn't been warned in the last 10 seconds, so add a warning
-            warn_id = await Warnings.add_warning(message.guild.id, message.author.id, 1, "[AUTO] spamming")
+            warn_id = await Warnings(self.bot).add_warning(message.guild.id, message.author.id, 1, "[AUTO] spamming")
             await message.channel.send(
                 f":warning: [`{warn_id}`] {message.author.mention}, you have been given a 1 point warning for spamming"
                 f"\nIf you keep spamming, you will get another warning!")
-        # else:
-        #     # a warning was issued in the last 10 seconds, so don't warn again
-        #     await message.channel.send("You have already been warned for spamming! Continue to spam and another "
-        #                                "warning will be issued")
-
-        print(f"{time.time() - start} seconds, gave warning: {warned}")
 
 
 def setup(bot):
