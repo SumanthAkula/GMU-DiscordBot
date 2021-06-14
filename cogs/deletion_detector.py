66004948ec5a03ff424071a2fa8a9752605ec06f
Sumo Akula <sumoa29@gmail.com>
2021-06-14 18:56:45 -0400

    switched to just sending deleted things directly to another channel instead of saving it in mongodb

--- cogs/deletion_detector.py
+++ cogs/deletion_detector.py
@@ -1,100 +1,61 @@
-import os
 import time
-import uuid
-from pathlib import Path
+from datetime import datetime
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 import main
 
-# constant path for the temporary files needed for recreating the attachments
-DELETED_ATTACHMENTS_PATH = path = "tmp/deleted_attachments/"
+from utils.log_channel_types import LogChannelType
 
 
 class DeletionDetector(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.cache = []
-        self.clear_cache.start()
-        self.clear_old_deletes.start()
-        Path(DELETED_ATTACHMENTS_PATH).mkdir(parents=True, exist_ok=True)  # create folders for the temp files
 
-    @tasks.loop(seconds=30)
-    async def clear_cache(self):
-        self.cache = []
-
-    @tasks.loop(minutes=1)
-    async def clear_old_deletes(self):
-        """
-        deletes deleted messages from the database if they were deleted 14 or more days ago.
-        """
-        threshold = time.time() - 604800 * 2
-        main.db.deleted_messages.delete_many(
-            {
-                "time_deleted": {
-                    "$lte": threshold
-                }
-            }
-        )
-
-    @commands.command(name="snipe")
-    async def __snipe_cmd(self, ctx: commands.Context):
-        if not self.cache:
-            await ctx.reply("Nothing to snipe!")
+    @commands.command(name="setdeletionlog", aliases=["sdl"])
+    @commands.guild_only()
+    async def __set_deletion_log_channel(self, ctx: commands.Context, channel: discord.TextChannel):
+        if ctx.guild.id != channel.guild.id:
+            await ctx.reply("The channel must be a channel from this server!")
             return
-        await ctx.send(self.cache[0])
-
-    @commands.command(name="getdeleted", aliases=["gd"])
-    async def __get_deleted_cmd(self, ctx: commands.Context, member: discord.Member, amount: int = 1):
-        messages = main.db.deleted_messages.find(
+        main.db.log_channels.replace_one(
             {
                 "guild_id": ctx.guild.id,
-                "author_id": member.id
-            }, limit=amount
-        ).sort("time_sent", -1)
-        messages = list(messages)
-        if not messages:
-            await ctx.send("There are no recently deleted messages from this user!")
-        for message in messages:
-            if message["content"]:
-                await ctx.send(message["content"])
-            if message["attachments"]:  # check if the message has attachments
-                for a in message["attachments"]:  # if so, load each one and send to discord
-                    file_name = f"G{ctx.guild.id}_{a['filename']}"
-                    with open(f"{DELETED_ATTACHMENTS_PATH}{file_name}", "wb") as fp:  # create a temporary file
-                        # decode the base64 string and write it back to the file
-                        fp.write(a["data"])
-                    await ctx.send(file=discord.File(DELETED_ATTACHMENTS_PATH + file_name))
-                    os.remove(DELETED_ATTACHMENTS_PATH + file_name)  # delete the temp file after sending it to discord
+                "log_type": LogChannelType.MessageDeletion.value
+            },
+            {
+                "guild_id": ctx.guild.id,
+                "log_type": LogChannelType.MessageDeletion.value,
+                "channel_id": channel.id
+            },
+            upsert=True
+        )
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
-        attachments = []
-        for a in message.attachments:
-            """
-            encode attachments as base64 strings to store them in the database easily
-            """
-            file = await a.to_file()
-            attachments.append(
-                {
-                    "attachment_id": a.id,
-                    "filename": a.filename,
-                    "type": a.content_type,
-                    "data": file.fp.read()
-                }
-            )
-        main.db.deleted_messages.insert_one(
+        if message.channel.type == discord.ChannelType.group or message.channel.type == discord.ChannelType.private:
+            return  # do not log deleted messages in DMs
+        channel_id = main.db.log_channels.find_one(
             {
-                "_id": str(uuid.uuid1()),
                 "guild_id": message.guild.id,
-                "author_id": message.author.id,
-                "content": message.content,
-                "attachments": attachments,
-                "time_sent": message.created_at.timestamp(),
-                "time_deleted": time.time()
+                "log_type": LogChannelType.MessageDeletion.value
             }
-        )
+        )["channel_id"]
+        channel = await self.bot.fetch_channel(channel_id)
+
+        attachments = []
+        if message.attachments:
+            for attachment in message.attachments:
+                orig = await attachment.to_file()
+                attachments.append(discord.File(fp=orig.fp, filename=attachment.filename))
+
+        await channel.send(f"```"
+                           f"author: {message.author.display_name}\n"
+                           f"time sent: {message.created_at}\n"
+                           f"time deleted: {datetime.fromtimestamp(time.time())}\n"
+                           f"content: {message.content}\n"
+                           f"```", files=None if not message.attachments else attachments)
 
     async def on_bulk_message_delete(self, messages: list[discord.Message]):
         pass  # TODO
