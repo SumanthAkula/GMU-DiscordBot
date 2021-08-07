d041a12d4778bf727e46609a013f3f3abd2561be
Sumo Akula <sumoa29@gmail.com>
2021-08-06 20:06:16 -0400

    logic for SelectMenu finished

---
+++ cogs/logger_channels.py
@@ -0,0 +1,78 @@
+import discord
+from discord.ext import commands
+
+import main
+from utils.database.collections import LOG_CHANNELS
+from utils.log_channel_types import LogChannelType
+
+
+class LoggerChannels(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group("channel", pass_context=True, invoke_without_command=True)
+    @commands.guild_only()
+    async def __channel_cmd(self, ctx: commands.Context):
+        """
+        You can set channels as 'logs' the bot can send messages to for moderation purposes
+        """
+        await ctx.reply(f"Run the `{ctx.prefix}help channel` command for more information on how to use this command")
+
+    @__channel_cmd.command("set", pass_context=True, invoke_without_command=True)
+    @commands.guild_only()
+    async def __set(self, ctx: commands.Context):
+        options: [discord.SelectOption] = []
+        for log_type in LogChannelType:
+            options.append(discord.SelectOption(label=log_type.name, default=False))
+
+        class LogTypeSelector(discord.ui.View):
+            def __init__(self):
+                super().__init__()
+                self.selected_type = None
+
+            @discord.ui.select(placeholder="Select what type of log you want to set", options=options)
+            async def selection_made(self, select_menu: discord.ui.Select, interaction: discord.Interaction):
+                if interaction.user != ctx.author:
+                    await interaction.response.send_message("Only the person who ran the command can "
+                                                            "select a log type!", ephemeral=True)
+                    return
+                self.selected_type = LogChannelType[select_menu.values[0]]
+                await interaction.message.edit(f"Tag the channel you would like to set as the "
+                                               f"{self.selected_type.name} log channel", view=None)
+
+            @property
+            def get_type(self):
+                return self.selected_type
+
+        view = LogTypeSelector()
+        original = await ctx.reply("Uhh idk what im doing :V", view=view)
+
+        def integration_check(_interaction: discord.Interaction):
+            return _interaction.user == ctx.author and \
+                   _interaction.channel == ctx.channel and \
+                   _interaction.application_id == self.bot.application_id and \
+                   _interaction.message == original
+        await self.bot.wait_for("interaction", check=integration_check)
+        await self.set_channel(ctx, view.get_type, channel)
+        print(f"type selected: {view.get_type}")
+
+    async def set_channel(self, ctx: commands.Context, log_type: LogChannelType, channel: discord.TextChannel):
+        if ctx.guild.id != channel.guild.id:
+            await ctx.reply("The channel must be a text channel from this server!")
+            return
+        main.db[LOG_CHANNELS].replace_one(
+            {
+                "guild_id": ctx.guild.id,
+                "log_type": log_type.value
+            },
+            {
+                "guild_id": ctx.guild.id,
+                "log_type": log_type.value,
+                "channel_id": channel.id
+            },
+            upsert=True
+        )
+
+
+def setup(bot):
+    bot.add_cog(LoggerChannels(bot))
