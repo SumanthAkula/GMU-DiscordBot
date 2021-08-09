ea2735f897b0c00781098a22d51ff07e3e9b8a2b
Sumo Akula <sumoa29@gmail.com>
2021-08-09 12:46:12 -0400

    created cog for detecting when people boost a server

---
+++ cogs/boost.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+from utils.log_channel_types import LogChannelType
+
+
+class Boost(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        # nitro_role = before.guild.premium_subscriber_role
+        nitro_role = before.guild.get_role(874326425979740171)  # TODO: set this to the actual role on release
+        if nitro_role is None:
+            return
+        if nitro_role not in before.roles and nitro_role in after.roles:
+            gen_chat: discord.TextChannel = \
+                await self.bot.get_cog("LoggerChannels").get_channel(self.bot, after.guild.id, LogChannelType.General)
+            embed = discord.Embed(title="AYYYY A NEW BOOSTER LETS GOOOOO", color=nitro_role.color)
+            embed.set_image(url="attachment://assets/booster_banner.PNG")
+            embed.description = after.mention
+            await gen_chat.send(file=discord.File("assets/booster_banner.PNG"), embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Boost(bot))
