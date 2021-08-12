c4273ddb4672fe4fd8d28aa7618fdab8c4196e7a
Sumo Akula <sumoa29@gmail.com>
2021-08-12 14:41:13 -0400

    reply to a message to uwuify it too

--- cogs/uwuifier.py
+++ cogs/uwuifier.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import discord
 import uwuify
 from discord.ext import commands
 
@@ -6,10 +9,17 @@ class Uwuifier(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def uwu(self, ctx: commands.Context, *, msg):
-        flags = uwuify.SMILEY
-        await ctx.send(uwuify.uwu(msg, flags=flags))
+    @commands.command(hidden=True)
+    async def uwu(self, ctx: commands.Context, *, msg: Optional[str]):
+        if ctx.message.reference is None and msg is None:
+            await ctx.reply("You have to either reply to a message with this command OR provide some text to uwuify as "
+                            "an argument")
+            return
+        if ctx.message.reference:
+            original: discord.Message = await ctx.message.channel.fetch_message(ctx.message.reference.message_id)
+            msg = original.content
+        flags = uwuify.SMILEY | uwuify.YU
+        await ctx.send(uwuify.uwu(msg, flags=uwuify.UwuifyFlag(flags)))
 
 
 def setup(bot):
