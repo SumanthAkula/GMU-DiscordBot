9e4be4f19bbed839c2a2578bdaa2001870583542
Sumo Akula <sumoa29@gmail.com>
2021-08-12 13:18:44 -0400

    reply to the command instead of just sending a message

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,3 +1,5 @@
+import asyncio.exceptions
+
 from discord.ext import commands
 from discord.ext.commands import errors
 
@@ -9,21 +11,21 @@ class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
         if isinstance(error, errors.MissingRequiredArgument):
-            await ctx.send(f"You are missing the `{error.param.name}` argument! "
+            await ctx.reply(f"You are missing the `{error.param.name}` argument! "
                            f"Run the help command to see how this command should be used")
         elif isinstance(error, errors.NoPrivateMessage):
-            await ctx.send("This command cannot be used in DMs!")
+            await ctx.reply("This command cannot be used in DMs!")
         elif isinstance(error, errors.PrivateMessageOnly):
-            await ctx.send("This command can only be used in DMs!")
+            await ctx.reply("This command can only be used in DMs!")
         elif isinstance(error, errors.CommandNotFound):
             pass
         elif isinstance(error, errors.CommandOnCooldown):
-            await ctx.send(f"This command is on a cooldown, "
+            await ctx.reply(f"This command is on a cooldown, "
                            f"try running that command again in {error.retry_after} second(s)")
         elif isinstance(error, errors.DisabledCommand):
             await ctx.send("You cannot run that command because it has been disabled")
         else:
-            await ctx.send(str(error))
+            await ctx.reply(str(error))
             raise error
 
 
