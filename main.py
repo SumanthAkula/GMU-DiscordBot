2955774c3645760c1f1e691274130802df97da3a
Sumo Akula <sumoa29@gmail.com>
2021-08-14 14:55:01 -0400

    WIP command disabler

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pymongo
 from pymongo import errors
 
 import secret  # super duper secret .py file used to store the bot token
+from utils.database.collections import DISABLED_COMMANDS
 
 bot_description = f"This is a bot made by the nerds over in the GMU Computer Science discord server!\n" \
                   f"This specific bot was developed by Sumo Akula on the notsumo_Dev branch"
@@ -35,6 +36,20 @@ for f in os.listdir("./cogs"):
 @bot.event
 async def on_ready():
     print("bot started!")
+
+
+@bot.check
+async def global_check(ctx: commands.Context):
+    data = db[DISABLED_COMMANDS].find_one(
+        {
+            "guild_id": ctx.guild.id,
+            "channel_id": ctx.channel.id,
+            "command_name": ctx.command.name
+        }
+    )
+    if data is None:
+        return True
+    return False
 
 
 if __name__ == "__main__":
