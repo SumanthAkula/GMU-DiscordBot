0134df657f9d371490444879485bf2ef8c47e065
SumanthAkula <sumoa29@gmail.com>
2021-08-22 18:08:36 -0400

    moved the modCheck to be above the cog class

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -5,32 +5,41 @@ import main
 from utils.database.collections import MOD_ROLES
 
 
-class Moderator(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+class NoModeratorRoleError(commands.CheckFailure):
+	pass
 
-	@staticmethod
-	async def moderator_role_check(ctx: commands.Context):
-		class NoModeratorRoleError(commands.CheckFailure):
-			pass
 
+def moderator_role_check():
+	def predicate(ctx: commands.Context):
 		role_data = main.db[MOD_ROLES].find_one(
 			{
 				"guild_id": ctx.guild.id
 			}
 		)
-		if role_data is None or ctx.author.get_role(role_data["role_id"]) is None:
-			raise NoModeratorRoleError
+		if ctx.guild.owner == ctx.author:
+			return True
+		elif role_data is None:
+			raise NoModeratorRoleError(":x: No moderator role has been set! Contact the server owner to get this "
+									   "fixed.")
+		elif ctx.author.get_role(role_data["role_id"]) is None:
+			raise NoModeratorRoleError(":x: Only moderators can run this command!")
 		return True
 
+	return commands.check(predicate)
+
+
+class Moderator(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
 	@commands.command()
-	@commands.check(moderator_role_check)
+	@moderator_role_check()
 	async def setmodrole(self, ctx: commands.Context, role_id: int):
 		role: discord.Role = ctx.guild.get_role(role_id)
 		if role is None:
 			await ctx.reply(":x: That role does not exist in this guild!")
 			return
-		result = main.db[MOD_ROLES].replace_one(
+		main.db[MOD_ROLES].replace_one(
 			{
 				"guild_id": ctx.guild.id
 			},
