99557571c95972c3b5a710007678bf211e0dc7e7
Sumo Akula <sumoa29@gmail.com>
2021-08-22 12:49:49 -0400

    remove global check

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 import pymongo
 
 import secret  # super duper secret .py file used to store the bot token
-from utils.database.collections import DISABLED_COMMANDS
 
 bot_description = f"This is a bot made by the nerds over in the GMU Computer Science discord server!\n" \
                   f"This specific bot was developed by Sumo Akula on the notsumo_Dev branch"
@@ -20,7 +19,7 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or(":V "),
                    activity=activity)
 
 # connect to database
-db = pymongo.MongoClient(secret.DB_REMOTE_URL, connectTimeoutMS=60000)["botdb"]
+db = pymongo.MongoClient(secret.DB_REMOTE_URL)["botdb"]
 
 # load all cogs in the ./cogs folder (that's pretty much every file that ends in .py in that folder)
 for f in os.listdir("./cogs"):
@@ -31,20 +30,6 @@ for f in os.listdir("./cogs"):
 @bot.event
 async def on_ready():
     print("bot started!")
-
-
-@bot.check
-async def global_check(ctx: commands.Context):
-    data = db[DISABLED_COMMANDS].find_one(
-        {
-            "guild_id": ctx.guild.id,
-            "channel_id": ctx.channel.id,
-            "command_name": ctx.command.name
-        }
-    )
-    if data is None:
-        return True
-    return False
 
 
 if __name__ == "__main__":
