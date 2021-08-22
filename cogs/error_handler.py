37295278ac8f62af28c559785befe4f7b0d1f763
SumanthAkula <sumoa29@gmail.com>
2021-08-22 18:08:04 -0400

    INDENT WITH TABS

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -5,32 +5,32 @@ from discord.ext.commands import errors
 
 
 class ErrorHandler(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
-        if isinstance(error, errors.MissingRequiredArgument):
-            await ctx.reply(f"You are missing the `{error.param.name}` argument! "
-                           f"Run the help command to see how this command should be used")
-        elif isinstance(error, errors.NoPrivateMessage):
-            await ctx.reply("This command cannot be used in DMs!")
-        elif isinstance(error, errors.PrivateMessageOnly):
-            await ctx.reply("This command can only be used in DMs!")
-        elif isinstance(error, errors.CommandNotFound):
-            pass
-        elif isinstance(error, errors.CommandOnCooldown):
-            await ctx.reply(f"This command is on a cooldown, "
-                           f"try running that command again in {error.retry_after} second(s)")
-        elif isinstance(error, errors.DisabledCommand):
-            await ctx.reply("You cannot run that command because it has been disabled")
-        elif isinstance(error, errors.CommandInvokeError):
-            if isinstance(error.original, asyncio.exceptions.TimeoutError):
-                await ctx.reply("The command you were trying to run timed out! ")
-        else:
-            await ctx.reply(str(error))
-            raise error
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+		if isinstance(error, errors.MissingRequiredArgument):
+			await ctx.reply(f"You are missing the `{error.param.name}` argument! "
+							f"Run the help command to see how this command should be used")
+		elif isinstance(error, errors.NoPrivateMessage):
+			await ctx.reply("This command cannot be used in DMs!")
+		elif isinstance(error, errors.PrivateMessageOnly):
+			await ctx.reply("This command can only be used in DMs!")
+		elif isinstance(error, errors.CommandNotFound):
+			pass
+		elif isinstance(error, errors.CommandOnCooldown):
+			await ctx.reply(f"This command is on a cooldown, "
+							f"try running that command again in {error.retry_after} second(s)")
+		elif isinstance(error, errors.DisabledCommand):
+			await ctx.reply("You cannot run that command because it has been disabled")
+		elif isinstance(error, errors.CommandInvokeError):
+			if isinstance(error.original, asyncio.exceptions.TimeoutError):
+				await ctx.reply("The command you were trying to run timed out! ")
+		else:
+			await ctx.reply(str(error))
+			raise error
 
 
 def setup(bot):
-    bot.add_cog(ErrorHandler(bot))
+	bot.add_cog(ErrorHandler(bot))
