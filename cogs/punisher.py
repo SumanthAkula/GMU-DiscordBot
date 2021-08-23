1fbe7298a029a8601807338f82b312e51688dc2c
Sumo Akula <sumoa29@gmail.com>
2021-08-22 23:00:17 -0400

    added functions to set and get the role that is assigned to mute people

--- cogs/punisher.py
+++ cogs/punisher.py
@@ -1,12 +1,11 @@
 import time
 from enum import Enum
+from typing import Union
 
 import discord
 from discord.ext import commands, tasks
-from discord.utils import get
-
 import main
-from utils.database.collections import PUNISHMENTS
+from utils.database.collections import PUNISHMENTS, MOD_ROLES
 
 
 class Punishment(Enum):
@@ -74,7 +73,7 @@ class Punisher(commands.Cog):
                     await self.revoke_temp_ban(punishment)
             elif punishment["type"] == Punishment.MUTE.value:
                 if now - punishment["time"] >= punishment["length"] * 3600:
-                    await self.revoke_temp_mute(punishment)
+                    await self.revoke_mute(punishment)
 
     # BANNING USERS
     @commands.command(name="ban")
@@ -138,10 +137,51 @@ class Punisher(commands.Cog):
         user: discord.User = await self.bot.fetch_user(ban["user_id"])
         await guild.unban(user, reason="[AUTO] temporary ban timer expired")
 
+    # MUTING MEMBERS
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def set_mute_role(self, ctx: commands.Context, role_id: int):
+        """
+        Set the role that will get assigned when a member is muted
+
+        Arguments:
+            role_id: the ID of the role to assign.
+        """
+        role: discord.Role = ctx.guild.get_role(role_id)
+        if role is None:
+            await ctx.reply(":x: That role does not exist in this guild!")
+            return
+        main.db[MOD_ROLES].replace_one(
+            {
+                "guild_id": ctx.guild.id
+            },
+            {
+                "guild_id": ctx.guild.id,
+                "role_id": role_id
+            },
+            upsert=True
+        )
+        await ctx.reply(f":white_check_mark: Muted role set to `{role.name}`!")
+
+    @staticmethod
+    async def get_mute_role(guild: discord.Guild) -> Union[discord.Role, None]:
+        data = main.db[MOD_ROLES].find_one(
+            {
+                "guild_id": guild.id
+            }
+        )
+        if data is None:
+            return None
+        else:
+            return guild.get_role(data["role_id"])
+
     @commands.command(name="mute")
     @commands.guild_only()
     @commands.has_permissions(ban_members=True)
     async def __mute_member(self, ctx: commands.Context, member: discord.Member, length: int, *, reason: str):
+        if await Punisher.get_mute_role(ctx.guild) is None:
+            await ctx.send(":x: There is no 'mute' role set for this server! Contact a server admin to have this fixed")
+            return
         await self.mute_member(ctx.guild, member, length, reason)
         await ctx.reply(f":white_check_mark: Muted {member.mention} for {length} hour(s)")
 
@@ -159,24 +199,23 @@ class Punisher(commands.Cog):
         if data is None:
             await ctx.reply(":x: That member is not muted")
             return
-        await self.revoke_temp_mute(data)
+        await self.revoke_mute(data, f"manually unmuted by {ctx.author.name}")
         await ctx.reply(f":white_check_mark: Unmuted {member.mention}")
 
-    # MUTING MEMBERS
     @staticmethod
     async def mute_member(guild: discord.Guild, member: discord.Member, length: int, reason: str):
         lenstr = f"for {length} hour(s)" if length >= 0 else "indefinitely"
-        role = get(guild.roles, name="muted")
+        role = await Punisher.get_mute_role(guild)
         await member.add_roles(role, reason=f"{reason}")
         await Punisher.__add_punishment_to_db(guild.id, member.id, Punishment.MUTE, length, reason)
         await member.send(f"You have been muted {lenstr} in the {guild.name} Discord server for the following reason(s)"
                           f"\n{reason}")
 
-    async def revoke_temp_mute(self, punishment: dict):
+    async def revoke_mute(self, punishment: dict, reason: str = "[AUTO] temporary mute expired"):
         guild: discord.Guild = await self.bot.fetch_guild(punishment["guild_id"])
         member: discord.Member = await guild.fetch_member(punishment["user_id"])
-        role = get(guild.roles, name="muted")
-        await member.remove_roles(role, reason="[AUTO] temporary mute expired")
+        role = await Punisher.get_mute_role(guild)
+        await member.remove_roles(role, reason=reason)
         main.db[PUNISHMENTS].delete_one(
             {
                 "guild_id": guild.id,
